b9c8355860dda6a2eacef348af05bbf863d94383
Justin Vititoe <jvit1985@yahoo.com>
2022-07-17 19:10:40 -0500

    updated apollo server and typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+// set up login in Mutation may also include username per user-controller function
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+    bookCount: Int
+}
+
+type Book {
+    _id: ID
+    description: String
+    authors: [Book]
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(username: String!, title: String!): User
+    deleteBook(username: String!, bookId: String!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs;
